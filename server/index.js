29a08c33a890634435129db8f8f4e02c25d35d42
parkoon <par_koon@naver.com>
2019-05-17 18:29:01 +0900

    개발중..

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.listen(3000, () => {
+    console.log(`Server is running on ${3000}`)
+})
